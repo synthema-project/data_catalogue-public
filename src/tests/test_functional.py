c7c72c594ba51b5f451789441484946f807f5608
Cesco16 <93782245+Cesco16@users.noreply.github.com>
2024-06-10 13:05:11 +0200

    Update test_functional.py

--- src/tests/test_functional.py
+++ src/tests/test_functional.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 from main import app
-from utils import create_connection
+from data_catalogue_utils import create_connection
 
 client = TestClient(app)
 
