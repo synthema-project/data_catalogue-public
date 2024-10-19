fdfe5a40d8ae742ecfdd3f3bc5b0f10e4cedfaed
casadeif <93782245+Cesco16@users.noreply.github.com>
2024-10-19 10:52:09 +0200

    Update test_functional2.py

--- src/tests/test_functional2.py
+++ src/tests/test_functional2.py
@@ -92,7 +92,7 @@ def test_delete_dataset_info():
     }
 
     # Delete the dataset info using the API
-    response = client.delete(f"{BASE_URL}/metadata", node=dataset_info_to_delete["node"], disease=dataset_info_to_delete["disease"], path=dataset_info_to_delete["path"])
+    response = client.delete(f"{BASE_URL}/metadata", removedatasetobject=dataset_info_to_delete)
     
     assert response.status_code == 200, f"Expected 200 OK but got {response.status_code}"
     assert response.json() == {"message": "Dataset '/updated/path/to/data' deleted successfully."}
