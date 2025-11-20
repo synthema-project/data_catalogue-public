da1ee4886cb602a24a25c70c7e0dc9cd994e4815
Francesco Casadei <93782245+Cesco16@users.noreply.github.com>
2025-11-20 13:19:22 +0100

    Update database.py

--- src/database.py
+++ src/database.py
@@ -11,8 +11,13 @@ connect_args = {}
 engine = create_engine(postgres_url, echo=True, connect_args=connect_args)
 
 def create_db_and_tables():
-    SQLModel.metadata.drop_all(engine)
-    SQLModel.metadata.create_all(engine)
+
+    SQLModel.metadata.drop_all(bind=engine, tables=[NodeDatasetInfo.__table__])  # Solo la tabella specifica
+
+    # Ricrea la tabella
+    SQLModel.metadata.create_all(bind=engine, tables=[NodeDatasetInfo.__table__])
+    #SQLModel.metadata.drop_all(engine)
+    #SQLModel.metadata.create_all(engine)
 
 from sqlalchemy.exc import ProgrammingError
 
@@ -32,6 +37,7 @@ def create_db_and_tables():
 
 def get_session():
     return Session(engine)
+
 
 
 
