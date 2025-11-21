72141226a5729e582dfb4408dd22e0b810f1575b
Francesco Casadei <93782245+Cesco16@users.noreply.github.com>
2025-11-21 15:27:43 +0100

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -101,7 +101,10 @@ def update_use_case(session, use_case, node, path):
     if record:
         # append only if not duplicated
         if new_entry not in record.datasets:
-            record.datasets = record.datasets + [new_entry]
+            #record.datasets = record.datasets + [new_entry]
+            record.datasets.append(new_entry)
+            session.add(record)
+            session.commit()
     else:
         record = UseCase(
             use_case=use_case,
@@ -374,6 +377,7 @@ async def get_user_requests_list(username: str, session: Session) -> List[dict]:
     except Exception as e:
 
         raise HTTPException(status_code=500, detail=str(e)) from e
+
 
 
 
