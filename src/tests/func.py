e5188871fb48c765350d6e8ef610e27d1da96caf
Francesco Casadei <93782245+Cesco16@users.noreply.github.com>
2026-02-04 23:19:56 +0100

    Update func.py

--- src/tests/func.py
+++ src/tests/func.py
@@ -2,18 +2,27 @@
 from fastapi.testclient import TestClient
 from main import app
 
+from main import app, get_session
+from sqlmodel import Session
+from unittest.mock import MagicMock
+
+def fake_session():
+    return MagicMock(spec=Session)
+
+app.dependency_overrides[get_session] = fake_session
+
 client = TestClient(app)
 
 def test_post_metadata():
     payload = {
-        "node":"n1",
-        "path":"file.csv",
-        "use_case":"covid",
-        "num_records":2,
-        "num_features":1,
-        "data_schema":{"a":"int"},
-        "dataset_metadata":{"title":"test"}
-    }
+    "node":"n1",
+    "path":"file.csv",
+    "use_case":"covid",
+    "num_records":2,
+    "num_features":1,
+    "schema":{"a":["int"]},
+    "metadata":{"title":"test"}
+}
 
     r = client.post("/metadata", json=payload)
     assert r.status_code == 200
