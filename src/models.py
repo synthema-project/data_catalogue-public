81acc98c26463c62f7c5b1545a1c2e10b2294100
Francesco Casadei <93782245+Cesco16@users.noreply.github.com>
2026-02-04 22:31:52 +0100

    Refactor models in src/models.py for clarity

--- src/models.py
+++ src/models.py
@@ -81,7 +81,7 @@ class NodeDatasetInfo(SQLModel, table=True, __tablename__="data_catalogue"):
     num_features: Optional[int] = None
     
     data_schema: Optional[Dict[str, Any]] = Field(
-        sa_column=Column(JSONB)
+        sa_column=Column(JSONType)#(JSONB)
     )
 
     dataset_metadata: Optional[DatasetMetadata] = Field(
@@ -197,6 +197,7 @@ class SyntheticDatasetGenerationRequestStatusTable(
             filters=[f.model_dump() for f in (req.filters or [])],
 
         )
+
 
 
 
