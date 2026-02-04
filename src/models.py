4ab5984589fdb717bf88fb62c58146697bdf94ea
Francesco Casadei <93782245+Cesco16@users.noreply.github.com>
2026-02-04 10:10:09 +0100

    Update models.py

--- src/models.py
+++ src/models.py
@@ -27,13 +27,11 @@ class NodeDatasetInfo(SQLModel, table=True, __tablename__="data_catalogue"):
     num_records: Optional[int] = None
     num_features: Optional[int] = None
     
-    schema: Optional[Dict[str, Any]] = Field(
+    data_schema: Optional[Dict[str, Any]] = Field(
         sa_column=Column(JSONB)
     )
 
-    
-    metadata: dict | None = Field(
-        default=None,
+    dataset_metadata: Optional[DatasetMetadata] = Field(
         sa_column=Column(JSON)
     )
 
@@ -146,6 +144,7 @@ class SyntheticDatasetGenerationRequestStatusTable(
             filters=[f.model_dump() for f in (req.filters or [])],
 
         )
+
 
 
 
